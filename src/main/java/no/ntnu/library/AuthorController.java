d48d038c321c740998b302200bf146dd3119d9f1
Mikkel <staveliem@gmail.com>
2023-02-01 19:24:28 +0100

    unit tests for AuthorController.java, new methods in AuthorController.java as well

--- src/main/java/no/ntnu/library/AuthorController.java
+++ src/main/java/no/ntnu/library/AuthorController.java
@@ -3,12 +3,14 @@ package no.ntnu.library;
 import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 import javax.swing.text.html.parser.Entity;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +25,14 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RestController
 public class AuthorController {
 
-    private List<Author> authors = new ArrayList<>();
+    private List<Author> authors;
 
     public AuthorController() {
         initializeData();
     }
 
     private void initializeData() {
+        authors = new ArrayList<>();
         authors.add(new Author(1, "Stephen", "King", 1947));
         authors.add(new Author(2, "Charles", "Dickens", 1812));
         authors.add(new Author(3, "George", "Orwell", 1903));
@@ -40,8 +43,8 @@ public class AuthorController {
     public ResponseEntity<Object> getAuthors() {
         ResponseEntity response;
 
-        if (this.authors.size() == 0) {
-            response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (this.authors.isEmpty()) {
+            response = new ResponseEntity<>("Collection contains no elements", HttpStatus.BAD_REQUEST);
         } else {
             response = new ResponseEntity<>(authors, HttpStatus.ACCEPTED);
         }
@@ -60,5 +63,32 @@ public class AuthorController {
             response = new ResponseEntity<>(author, HttpStatus.BAD_REQUEST);
         }
         return response;
+    }
+
+    @Operation(summary = "Delete author", description = "Deletes a author from the collection")
+    @DeleteMapping("/authors")
+    public ResponseEntity<Object> deleteAuthorMap(@RequestBody Author author) {
+        ResponseEntity response;
+
+        return response;
+    }
+
+    public void deleteAuthor(String name) {
+        Author author = null;
+
+        Iterator<Author> it = authors.iterator();
+
+        while (it.hasNext()) {
+            Author a = it.next();
+
+            if (a.getFirstName().equals(name)) {
+                author = a;
+                authors.remove(author);
+            }
+        }
+    }
+
+    public List<Author> getList() {
+        return this.authors;
     }
 }
---
+++ src/test/java/no/ntnu/library/AuthorControllerTests.java
@@ -0,0 +1,21 @@
+package no.ntnu.library;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AutoProxyRegistrar;
+
+public class AuthorControllerTests {
+
+    private AuthorController authorController = new AuthorController();
+
+    @Test
+    void testDeleteAuthor_positive() {
+        authorController.deleteAuthor("Stephen");
+        assertEquals(2, authorController.getList().size());
+    }
+
+}
